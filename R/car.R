3d362079f6f00e84ac6c9d682eed953896fc5bc3
Zbigniew Marczewski <zbigniew.marczewski@gazeta.pl>
2017-05-03 15:14:46 +0200

    testthat

--- R/car.R
+++ R/car.R
@@ -4,7 +4,9 @@
 #' @param STOP character. An adresss of destination.
 #' @return car_travel_cost returns a list containing following components
 #' @examples
-#' car_travel_cost(START="plac Defilad 1, 00-901 Warszawa Polska", STOP="Stanisława Kostki Potockiego 10/16, Warszawa Polska", combustion=8, fuel_price=4.5, travelers_number = 1)
+#' car_travel_cost(START="plac Defilad 1, 00-901 Warszawa Polska",
+#' STOP="Stanisława Kostki Potockiego 10/16, Warszawa Polska",
+#' combustion=8, fuel_price=4.5, travelers_number = 1)
 car_travel_cost <- function(START, STOP, combustion, fuel_price, travelers_number = 1) {
     # obsluga wyjatkow START & STOP
     if (length(START) != 1 | length(STOP) != 1) {
@@ -48,13 +50,22 @@ car_travel_cost <- function(START, STOP, combustion, fuel_price, travelers_numbe
     }
     FROM <- stringi::stri_enc_toutf8(START)
     TO <- stringi::stri_enc_toutf8(STOP)
-    Google_Maps_Query <- mapdist(FROM, TO, mode = "driving")
+    Google_Maps_Query <- ggmap::mapdist(FROM, TO, mode = "driving")
     benzyna <- Google_Maps_Query$km/100 * combustion
     koszt_benzyny <- fuel_price * benzyna
     koszt_benzyny_na_osobe <- fuel_price * benzyna/travelers_number
-    return(list(dystans = Google_Maps_Query$km, ilosc_benzyny = benzyna, koszt_benzyny = koszt_benzyny, koszt_benzyny_na_osobe = koszt_benzyny_na_osobe))
+    return(list(distance = Google_Maps_Query$km, fuel_amount = benzyna, fuel_cost = koszt_benzyny, fuel_price_per_traveler = koszt_benzyny_na_osobe))
 }
-
+#' Calculate total car travel cost
+#'
+#' @param START character. An adress of starting point.
+#' @param STOP character. An adresss of destination.
+#' @return car_costs returns a cost of car travel pre traveler
+#' @examples
+#' car_travel_cost(START="plac Defilad 1, 00-901 Warszawa Polska",
+#' STOP="Stanisława Kostki Potockiego 10/16, Warszawa Polska",
+#' combustion=8, fuel_price=4.5, travelers_number = 1,
+#' insurence_fee=1000, parking_time=8)
 
 car_costs <- function(START, STOP, travelers_number, combustion, fuel_price, insurence_fee, parking_time) {
     if (length(insurence_fee) != 1) {
@@ -70,6 +81,6 @@ car_costs <- function(START, STOP, travelers_number, combustion, fuel_price, ins
         stop("insurence_fee musi być wektorem o długości 1 (skalar)")
     }
     car_total_cost <- car_travel_cost(START = START, STOP = STOP, travelers_number = travelers_number, combustion = combustion,
-        fuel_price = fuel_price) + insurence_fee/365 + cost_of_parking(parking_time = parking_time)
+        fuel_price = fuel_price)$fuel_price_per_traveler + insurence_fee/365 + cost_of_parking(parking_time = parking_time)
     return(car_total_cost)
 }
--- tests/testthat/testcar.R
+++ tests/testthat/testcar.R
@@ -1,20 +1,23 @@
 library(WarsawTransport)
 context("car_travel_cost")
 
-test_that("car_travel_cost is number of characters", {
-  expect_equal(car_travel_cost(START=, STOP=, combustion=1, fuel_price=1), 1)
-  expect_equal(car_travel_cost("ab"), 2)
-  expect_equal(car_travel_cost("abc"), 3)
+test_that("car_travel_cost correct results", {
+  expect_equal(car_travel_cost(START = "Warszawa", STOP="Lublin",combustion = 1, fuel_price = 1)$distance, 169.288)
+  expect_equal(car_travel_cost(START = "Warszawa", STOP="Lublin",combustion = 1, fuel_price = 1)$fuel_amount, 1.69288)
+  expect_equal(car_travel_cost(START = "Warszawa", STOP="Lublin",combustion = 1, fuel_price = 1)$fuel_cost, 1.69288)
+  expect_equal(car_travel_cost(START = "Warszawa", STOP="Lublin",combustion = 1, fuel_price = 1)$fuel_price_per_traveler, 1.69288)
+  # expect_equal(car_travel_cost("ab"), 2)
+  # expect_equal(car_travel_cost("abc"), 3)
 })
 
-test_that("car_travel_cost of factor is length of level", {
-  expect_equal(car_travel_cost(factor("a")), 1)
-  expect_equal(car_travel_cost(factor("ab")), 2)
-  expect_equal(car_travel_cost(factor("abc")), 3)
-})
-
-test_that("car_travel_cost of missing is missing", {
-  expect_equal(car_travel_cost(NA), NA_integer_)
-  expect_equal(car_travel_cost(c(NA, 1)), c(NA, 1))
-  expect_equal(car_travel_cost("NA"), 2)
-})
+# test_that("car_travel_cost of factor is length of level", {
+#   expect_equal(car_travel_cost(factor("a")), 1)
+#   expect_equal(car_travel_cost(factor("ab")), 2)
+#   expect_equal(car_travel_cost(factor("abc")), 3)
+# })
+# 
+# test_that("car_travel_cost of missing is missing", {
+#   expect_equal(car_travel_cost(NA), NA_integer_)
+#   expect_equal(car_travel_cost(c(NA, 1)), c(NA, 1))
+#   expect_equal(car_travel_cost("NA"), 2)
+# })
