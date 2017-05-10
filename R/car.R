7c9b2efda94990cf6163f18e15d1a68fcf7234a0
Zbigniew Marczewski <zbigniew.marczewski@gazeta.pl>
2017-05-11 00:40:14 +0200

    translation2

--- R/car.R
+++ R/car.R
@@ -19,43 +19,43 @@
 car_travel_cost <- function(start_point, finish_point, fuel_consumption, fuel_price, travelers_number = 1) {
     # Exception handling for start_point and finish_point arguments
     if (length(start_point) != 1 | length(finish_point) != 1) {
-      stop("Error: argument start_point or finish_point is not character. Please enter start_point and finish_point as character vectors length 1")
+      stop("Error: argument start_point or finish_point is not character. Please enter start_point and finish_point as character vectors of length 1")
     }
     if ((is.character(start_point) & is.character(finish_point)) == FALSE) {
-      stop("Error: argument start_point or finish_point is not character. Please enter start_point and finish_point as character vectors length 1")
+      stop("Error: argument start_point or finish_point is not character. Please enter start_point and finish_point as character vectors of length 1")
     }
     # Exception handling for travelers_number argument
     if (length(travelers_number) != 1) {
-      stop("Error: argument travelers_number is not numeric. Please enter travelers_number as numeric vector length 1")
+      stop("Error: argument travelers_number is not numeric. Please enter travelers_number as numeric vector of length 1")
     }
     if (is.numeric(travelers_number) == FALSE) {
-      stop("Error: argument travelers_number is not numeric. Please enter travelers_number as numeric vector length 1")
+      stop("Error: argument travelers_number is not numeric. Please enter travelers_number as numeric vector of length 1")
     }
     if (travelers_number < 0 | travelers_number > 10) {
-      stop("Error: argument travelers_number is . Please enter ...")
+      stop("Error: argument travelers_number is smaller then 0 or bigger then 10. Please enter integer from range [0,10]")
     }
     if ((travelers_number%%1) > 0) {
-      stop("travelers_number musi być liczbą całkowitą")
+      stop("Error: argument travelers_number is not integer.")
     }
-    # obsluga wyjatkow fuel_consumption
+    # Exception handling for fuel_consumption argument
     if (length(fuel_consumption) != 1) {
-      stop("fuel_consumption musi byc pojedyncza liczba - wektorem klasy 'numeric' o dlugosic 1")
+      stop("Error: argument fuel_consumption is not numeric. Please enter fuel_consumption as numeric vector of length 1")
     }
     if (is.numeric(fuel_consumption) == FALSE) {
-      stop("fuel_consumption musi być klasy 'numeric'")
+      stop("Error: argument fuel_consumption is not numeric. Please enter fuel_consumption as numeric vector of length 1")
     }
     if (fuel_consumption <= 0 | fuel_consumption > 15) {
-      stop("fuel_consumption musi sie zawierac w przedziale (0,15]")
+      stop("Error: argument fuel_consumption is smaller or equel to 0 or bigger then 10. Please enter numeric from range (0,15]")
     }
-    # obsluga wyjatkow fuel_price
+    # Exception handling for fuel_price argument
     if (length(fuel_price) != 1) {
-      stop("fuel_price musi byc pojedyncza liczba - wektorem klasy 'numeric' o dlugosic 1")
+      stop("Error: argument fuel_price is not numeric. Please enter fuel_price as numeric vector of length 1")
     }
     if (is.numeric(fuel_price) == FALSE) {
-      stop("fuel_price musi byc klasy 'numeric'")
+      stop("Error: argument fuel_price is not numeric. Please enter fuel_price as numeric vector of length 1")
     }
     if (fuel_price <= 0 | fuel_price > 10) {
-      stop("fuel_price musi byc z zakresu (0,10]")
+      stop("Error: argument fuel_price is smaller then 0 or bigger then 10. Please enter numeric from range (0,10]")
     }
     from <- stringi::stri_enc_toutf8(start_point)
     to <- stringi::stri_enc_toutf8(finish_point)
@@ -83,16 +83,16 @@ car_travel_cost <- function(start_point, finish_point, fuel_consumption, fuel_pr
 #' @export
 car_costs <- function(start_point, finish_point, travelers_number, fuel_consumption, fuel_price, insurence_fee, parking_time) {
     if (length(insurence_fee) != 1) {
-      stop("insurence_fee musi być pojedyncza liczba - wektorem klasy 'numeric' o dlugosic 1")
+      stop("Error: argument insurence_fee is not numeric. Please enter insurence_fee as numeric vector of length 1")
     }
     if (is.numeric(insurence_fee) == FALSE) {
-      stop("insurence_fee musi być klasy 'numeric'")
+      stop("Error: argument insurence_fee is not numeric. Please enter insurence_fee as numeric vector of length 1")
     }
     if (insurence_fee <= 0 | insurence_fee > 15000) {
-      stop("insurence_fee musi z zakresu (0,15 000]")
+      stop("Error: argument insurence_fee is smaller or equal to 0 or greater than 10. Please enter numeric from range (0,15000]")
     }
     if (length(insurence_fee) != 1) {
-      stop("insurence_fee musi być wektorem o długości 1 (skalar)")
+      stop("Error: argument insurence_fee is not numeric. Please enter insurence_fee as numeric vector of length 1")
     }
     car_total_cost <- car_travel_cost(start_point = start_point, finish_point = finish_point, travelers_number = travelers_number, fuel_consumption = fuel_consumption,
         fuel_price = fuel_price)$fuel_cost_per_traveler + insurence_fee/365 + cost_of_parking(parking_time = parking_time)
