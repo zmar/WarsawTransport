8141e9b39a4b8d34c2b0c4574c945aedf2c9a0c6
Zbigniew Marczewski <zbigniew.marczewski@gazeta.pl>
2017-05-12 00:03:51 +0200

    tests1

--- tests/testthat/test_cost_of_parking.R
+++ tests/testthat/test_cost_of_parking.R
@@ -1,13 +1,23 @@
 library(WarsawTransport)
 context("cost_of_parking")
 
-test_that("cost_of_parking correct results", {
-  expect_equal(cost_of_parking(1), 3)
+test_that("parking_time - correct input", {
+  # parking_time equel to 1
+  expect_equal(cost_of_parking(parking_time = 1), 3)
+  # the smallest valid value of the parking_time - 0
+  expect_equal(cost_of_parking(parking_time = 0), 0)
+  # the greatest valid value of the parking_time - 24
+  expect_equal(cost_of_parking(parking_time = 24), 73.8)
 })
 
-test_that("cost_of_parking incorrect input", {
-  expect_error(cost_of_parking(25))
-  expect_error(cost_of_parking(-1))
-  expect_error(cost_of_parking("1"))
+test_that("cost_of_parking - handling errors", {
+  # parking_time beyound the greatest valid value
+  expect_error(cost_of_parking(parking_time = 25))
+  # parking_time below the smallest valid value
+  expect_error(cost_of_parking(parking_time = -1))
+  # parking_time as character
+  expect_error(cost_of_parking(parking_time = "1"))
+  # parking_time as vector of length 2
+  expect_error(cost_of_parking(parking_time = c(1,2)))
 })
 
--- tests/testthat/test_ztm_costs.R
+++ tests/testthat/test_ztm_costs.R
@@ -1,16 +1,25 @@
 library(WarsawTransport)
 context("tickets_costs_daily_ZTM")
 
-test_that("tickets_costs_daily_ZTM correct results", {
+test_that("tickets_costs_daily_ZTM - correct input", {
+  # correct input - results of number_of_tickets, tickets_cost and ztm_ticket
+  # number_of_tickets results is a numeric object
   expect_equal(tickets_costs_daily_ZTM(travel_time = 30, discount=TRUE, zone=1, varsovian=TRUE)$number_of_tickets, 1)
+  # tickets_cost results ia a numeric object
   expect_equal(tickets_costs_daily_ZTM(travel_time = 30, discount=TRUE, zone=1, varsovian=TRUE)$tickets_cost, 2.2)
+  # ztm_ticket results is a data.frame object
   expect_is(tickets_costs_daily_ZTM(travel_time = 30, discount=TRUE, zone=1, varsovian=TRUE)$ztm_ticket, "data.frame")
 })
 
-test_that("tickets_costs_daily_ZTM incorrect results", {
+test_that("tickets_costs_daily_ZTM errors - handling character input for number_of_tickets", {
+  # number_of_tickets output
+  ##  input - travel_time as character
   expect_error(tickets_costs_daily_ZTM(travel_time = "30", discount=TRUE, zone=1, varsovian=TRUE)$number_of_tickets)
+  ##  input - discount as character
   expect_error(tickets_costs_daily_ZTM(travel_time = 30, discount="TRUE", zone=1, varsovian=TRUE)$number_of_tickets)
+  ##  input - zone as character
   expect_error(tickets_costs_daily_ZTM(travel_time = 30, discount=TRUE, zone="1", varsovian=TRUE)$number_of_tickets)
+  ##  input - varsovian  as character
   expect_error(tickets_costs_daily_ZTM(travel_time = 30, discount=TRUE, zone=1, varsovian="TRUE")$number_of_tickets)
   expect_error(tickets_costs_daily_ZTM(travel_time = "30", discount=TRUE, zone=1, varsovian=TRUE)$tickets_cost)
   expect_error(tickets_costs_daily_ZTM(travel_time = 30, discount="TRUE", zone=1, varsovian=TRUE)$tickets_cost)
